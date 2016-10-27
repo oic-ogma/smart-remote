387a4277ed1ff9f2dca4c66038228f7d6fb0cab5
Davis Kemlers <daviskemlers@gmail.com>
2016-10-27 11:14:08 +0900

    9 - メールテンプレートとi18nの追加
    問題
    メールを送る為にテンプレートとi18nの翻訳が必要。
    
    解決策
    テンプレート文をprivateのフォルダーで言語ごとに保存。新しいi18nファイルの追加。

---
+++ i18n/email_templates.js
@@ -0,0 +1,19 @@
+i18n.addTranslations('en', {
+  emailTemplates: {
+    siteName: "Smart remote",
+    from: "Smart remote support <support@smart-remote.tech>",
+    enrollAccount: {
+      subject: "[Smart remote] Verify Your Email Address",
+    },
+  },
+});
+
+i18n.addTranslations('ja', {
+  emailTemplates: {
+    siteName: "スマートリモート",
+    from: "スマートリモートサポート <support@smart-remote.tech>",
+    enrollAccount: {
+      subject: "[スマートリモート] メールアドレスの確認",
+    },
+  },
+});
