7df5350f5fc5174390bad4882a11616f3ba9e638
Tyokopai <take353@gmail.com>
2016-11-29 15:42:23 +0900

    8 - update register page
    説明
    registerページの変更
    
    解決策
    バリデーションチェックがなかったのでcomponentを追加
    スタイルの変更はcssがsassで行うのでbootstrapのColタグ以外を削除
    emailとconfirmEmailの値が正しくないときにsendEnrollmentEmailをコールしないように変更

--- imports/ui/layouts/Register.jsx
+++ imports/ui/layouts/Register.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import i18n from 'meteor/universe:i18n';
 import LanguageSelector from '../components/LanguageSelector.jsx';
+import Validation from 'react-validation';
+import '../../api/validator/form_validator';
+import {FormGroup, Col} from 'react-bootstrap';
 
-export default class Login extends React.Component {
+export default class Register extends React.Component {
   constructor() {
     super(...arguments);
     this.state = {
@@ -27,24 +30,31 @@ export default class Login extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     let emailVar = event.target.registerEmail.value;
-    Meteor.call('sendEnrollmentEmail', i18n.getLocale(), emailVar, (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
+    let confirmEmailVar = event.target.confirmEmail.value;
+    if (emailVar === confirmEmailVar) {
+      Meteor.call('sendEnrollmentEmail', i18n.getLocale(), emailVar, (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    }
   }
+
   render() {
     return (
       <div>
         <LanguageSelector/>
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <div className="form-group col-md-offset-4 col-md-4">
-            <input  type="email" name="registerEmail" placeholder={i18n.getTranslation('register', 'email')}></input>
-          </div>
-          <div className="form-group col-md-offset-4 col-md-4">
-            <input className="btn btn-success" type="submit" value={i18n.getTranslation('register', 'register')}></input>
-          </div>
-        </form>
+        <Validation.components.Form onSubmit={this.handleSubmit.bind(this)}>
+          <FormGroup role="form">
+          <Col mdOffset={4} md={4} xs={4}>
+          <Validation.components.Input id="input-email" type="email" value='' name="registerEmail" placeholder={i18n.getTranslation('register', 'email')} validations={['required', 'email']}/>
+          <Validation.components.Input id="input-confirm-email" type="email" value='' name="confirmEmail" placeholder={i18n.getTranslation('register', 'confirmEmail')} validations={['confirmEmail']}/>
+          </Col>
+          <Col mdOffset={5} md={2} xs={4}>
+          <Validation.components.Button>{i18n.getTranslation('register', 'register')}</Validation.components.Button>
+          </Col>
+          </FormGroup>
+        </Validation.components.Form>
       </div>
     );
   }
