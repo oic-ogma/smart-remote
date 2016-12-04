13d2c1cdad81d5cd35b1a5fca12927774198e075
Tyokopai <take353@gmail.com>
2016-12-04 15:32:33 +0900

    8 - add i18n files
    説明
    各翻訳用のファイル作成
    
    解決策
    フォームに関連する翻訳文をまとめた
    サインインの翻訳文追加

---
+++ i18n/form.js
@@ -0,0 +1,27 @@
+i18n.addTranslations('en', {
+  form: {
+    signIn: "Sign In",
+    email: "Email",
+    confirmEmail: "Confirm email",
+    registerBtn: "Send register email",
+    password: "Password",
+    confirmPassword: "Confirm Password",
+    enrollBtn: "Register",
+    country: 'Country',
+    city: 'City',
+  },
+});
+
+i18n.addTranslations('ja', {
+  form: {
+    signIn: "サインイン",
+    email: "メールアドレス",
+    confirmEmail: "メールアドレスの確認",
+    registerBtn: "登録メール送信",
+    password: "パスワード",
+    confirmPassword: "パスワードの確認",
+    enrollBtn: "登録",
+    country: '国',
+    city: '都市',
+  },
+});
