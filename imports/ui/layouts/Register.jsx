24748c0e4542d1fd7e62b419387b298f41361bba
Tyokopai <take353@gmail.com>
2016-12-04 15:25:46 +0900

    8 - update register page
    説明
    インデント変更
    
    解決策
    見づらかったのでインデント変更

--- imports/ui/layouts/Register.jsx
+++ imports/ui/layouts/Register.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import i18n from 'meteor/universe:i18n';
-import LanguageSelector from '../components/LanguageSelector.jsx';
+import LanguageSelector from '../components/LanguageSelector';
 import Validation from 'react-validation';
 import '../../api/validator/form_validator';
 import {FormGroup, Col} from 'react-bootstrap';
@@ -45,15 +45,21 @@ export default class Register extends React.Component {
       <div>
         <LanguageSelector/>
         <Validation.components.Form onSubmit={this.handleSubmit.bind(this)}>
-          <FormGroup role="form">
-          <Col mdOffset={4} md={4} xs={4}>
-          <Validation.components.Input id="input-email" type="email" value='' name="registerEmail" placeholder={i18n.getTranslation('register', 'email')} validations={['required', 'email']}/>
-          <Validation.components.Input id="input-confirm-email" type="email" value='' name="confirmEmail" placeholder={i18n.getTranslation('register', 'confirmEmail')} validations={['confirmEmail']}/>
-          </Col>
-          <Col mdOffset={5} md={2} xs={4}>
-          <Validation.components.Button>{i18n.getTranslation('register', 'register')}</Validation.components.Button>
-          </Col>
-          </FormGroup>
+          <Validation.components.Input
+            id="input-email"
+            type="email"
+            value=''
+            name="registerEmail"
+            placeholder={i18n.getTranslation('form', 'email')}
+            validations={['required', 'email']}/>
+          <Validation.components.Input
+            id="input-confirm-email"
+            type="email"
+            value=''
+            name="confirmEmail"
+            placeholder={i18n.getTranslation('form', 'confirmEmail')}
+            validations={['confirmEmail']}/>
+          <Validation.components.Button>{i18n.getTranslation('form', 'registerBtn')}</Validation.components.Button>
         </Validation.components.Form>
       </div>
     );
