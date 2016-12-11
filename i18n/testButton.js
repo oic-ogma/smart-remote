b1e61ace6cdd688137842d03cbf3eefab8673c44
Hideyasu <kutuzov1228@gmail.com>
2016-12-11 17:20:57 +0900

    16 - alertの削除
    説明
    alertの削除
    
    解決策
    以上のことをした

--- i18n/testButton.js
+++ i18n/testButton.js
@@ -1,13 +1,11 @@
 i18n.addTranslations('en', {
   irTest: {
     testButton: "Test send",
-    testSuccess: "IR was sent",
   },
 });
 
 i18n.addTranslations('ja', {
   irTest: {
     testButton: "テスト送信",
-    testSuccess: "赤外線を送信しました",
   },
 });
