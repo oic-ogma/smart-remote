2347151c7d1908b2b1a2a7513759622c149b3423
Hideyasu <kutuzov1228@gmail.com>
2016-12-11 17:47:06 +0900

    slide-burgerの追加

--- imports/ui/layouts/AddButtonPanel.jsx
+++ imports/ui/layouts/AddButtonPanel.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import { slide as Menu } from 'react-burger-menu';
+import { MenuItem } from 'react-bootstrap';
 
 export default class AddButtonPanel extends React.Component {
+  showLeft() {
+    this.refs.left.show();
+  }
+
   render() {
     return (
       <div>
-        
+        <button onClick={this.showLeft}>Show Left Menu!</button>
+
+        <Menu ref="left" alignment="left">
+          <MenuItem hash="first-page">First Page</MenuItem>
+          <MenuItem hash="second-page">Second Page</MenuItem>
+          <MenuItem hash="third-page">Third Page</MenuItem>
+        </Menu>
       </div>
     );
   }
