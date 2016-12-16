67501ce5b5b6d1b0d8441e01f07760e1c56d0ae5
youmjww <b4234@oic.jp>
2016-12-16 12:19:38 +0900

    wip

--- imports/ui/layouts/AddButtonPanel.jsx
+++ imports/ui/layouts/AddButtonPanel.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Slider from '../components/Slider';
+import MyPageLarge from './MyPageLarge.jsx';
+import MyPageSmall from './MyPageSmall.jsx';
 
 export default class AddButtonPanel extends React.Component {
+
+  hoge() {
+    if (this.props.size === "lage") {
+      return <MyPageLarge/>;
+    } else {
+      return <MyPageSmall/>;
+    }
+  }
 
   render() {
     return (
         <div>
           <Slider/>
           <ul>
-            <li>morimoto</li>
-            <li>morimoto</li>
+            <button type="submit">morimoto1</button>
             <li>morimoto</li>
             <li>morimoto</li>
             <li>morimoto</li>
@@ -22,3 +31,7 @@ export default class AddButtonPanel extends React.Component {
     );
   }
 }
+
+AddButtonPanel.propTypes = {
+  size: React.PropTypes.string,
+};
