02f8bf92183c5e5a7f63f602225570fdbbad4673
Tyokopai <take353@gmail.com>
2016-12-20 13:59:45 +0900

    48 - Topページの作成
    説明
    トップページの作成
    
    解決策
    トップページとレイアウトの作成、ルーターの設定

---
+++ imports/ui/layouts/Top.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import i18n from 'meteor/universe:i18n';
+import Header from '../../ui/components/Header';
+export default class Top extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      locale: i18n.getLocale(),
+    };
+    this.onLocale = this.onLocale.bind(this);
+  }
+
+  onLocale(locale) {
+    this.setState({locale});
+  }
+
+  componentWillMount() {
+    i18n.onChangeLocale(this.onLocale);
+  }
+
+  componentWillUnmount() {
+    i18n.offChangeLocale(this.onLocale);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    location.href = '/sign-in';
+  }
+  render() {
+    return (
+      <div>
+        <Header/>
+        <div className="top-box">
+          <h1>{i18n.getTranslation('top', 'title1')}</h1>
+          <h1>{i18n.getTranslation('top', 'title2')}</h1>
+          <h3>{i18n.getTranslation('top', 'subTitle')}</h3>
+        </div>
+        <div className="top-button">
+          <button
+            type="button"
+            href="sign-in"
+            className="button-style"
+            onClick={this.handleSubmit.bind(this)}>
+              {i18n.getTranslation('signIn', 'signUp')}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
