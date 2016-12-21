f8041e53ba0f80e95272c8d3905c9e236b528fa6
Tyokopai <take353@gmail.com>
2016-12-21 21:19:33 +0900

    58 - フォームの成功メッセージ追加
    説明
    フォームの成功メッセージを追加
    
    解決策
    ポップアップ機能の追加と翻訳文の追加、フォームリセットの追加

---
+++ i18n/alert.js
@@ -0,0 +1,25 @@
+i18n.addTranslations('en', {
+  alert: {
+    success: {
+      register: "You have sent comfirm e-mail.",
+      enroll: "User registration is complete.",
+    },
+    error: {
+      register: "Already registered email address.",
+      enroll: "Already registered.",
+    },
+  },
+});
+
+i18n.addTranslations('ja', {
+  alert: {
+    success: {
+      register: "確認用メールを送信しました",
+      enroll: "ユーザー登録が完了しました",
+    },
+    error: {
+      register: "既に登録されたメールアドレスです",
+      enroll: "既に登録されています",
+    },
+  },
+});
