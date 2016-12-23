5da3b01ad910c48e1f2817396a10cf1714f2a77e
Davis Kemlers <daviskemlers@gmail.com>
2016-12-23 21:05:41 +0900

    50 - end-to-endテストをmocha.jsに変えた
    問題
    現在のe2eテストケースはcucumber.jsになって、単体テストはmocha.jsになってた。
    mocha.jsの方は日本語でテストケースをかきやすいので、全部mochaに統一させたい。
    
    解決策
    現在のテストを全部mocha.jsに変えて、runスクリプトとcircle.ymlの設定をmochaに対応
    させた。
    mocha.jsのe2eテストと単体テストとコンフリクトしてしもうて、e2eテストを'tests'と
    いうmeteorに無視されるフォルダーに移動した。

---
+++ tests/features/language_selector.js
@@ -0,0 +1,19 @@
+describe('言語切り替え', function() {
+  it('アプリケーションの言語を日本語に変える', function() {
+    browser.url('http://localhost:3000');
+    browser.waitForExist("#language-selector");
+    browser.click("#language-selector");
+    browser.click("#lang-ja");
+
+    expect(browser.getText("#language-selector")).to.equal("ja");
+  });
+
+  it('アプリケーションの言語を英語に変える', function() {
+    browser.url('http://localhost:3000');
+    browser.waitForExist("#language-selector");
+    browser.click("#language-selector");
+    browser.click("#lang-en");
+
+    expect(browser.getText("#language-selector")).to.equal("en");
+  });
+});
