6a7b3c47db3bcbe0935fab66f2e3b1352eda10fe
youmjww <b4234@oic.jp>
2017-01-13 16:22:48 +0900

    22 - publicationsを追加
    説明
    publicationsが無く、DBのデータを持ってこれなかった
    
    解決策
    publicationsを追加

---
+++ imports/api/ButtonLayout/server/publications.js
@@ -0,0 +1,12 @@
+import { ButtonLayout } from '../ButtonLayout';
+
+Meteor.publish("ButtonLayout", function() {
+  if (this.userId) {
+    return ButtonLayout.find({
+      userId: this.userId
+    }
+      );
+  } else {
+    this.ready();
+  }
+});
