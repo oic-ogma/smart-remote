4498c410c7458fa87a268573b142c05b984464ea
gucchii <b4247@oic.jp>
2017-01-13 11:36:23 +0900

    17 - photon情報追加ページのレイアウト変更
    　問題
    　　photon情報登録ページのレイアウトが未修正だった。
    
    　解決策
    　　AddPhoton.jsxのレイアウトの修正

--- imports/ui/layouts/AddPhoton.jsx
+++ imports/ui/layouts/AddPhoton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import i18n from 'meteor/universe:i18n';
+import Header from '../components/Header';
 import Validation from 'react-validation';
 import '../../api/validator/form_validator';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
-import { Col } from 'react-bootstrap';
+import {Grid, Col, Row } from 'react-bootstrap';
 import Alert from 'react-s-alert';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/genie.css';
@@ -45,30 +46,36 @@ export default class AddPhoton extends TrackerReact(React.Component) {
       i18n.setLocale(Meteor.user().profile.language);
       return (
         <div>
-          <Validation.components.Form onSubmit={this.handleSubmit.bind(this)} >
-            <div>{i18n.getTranslation('addPhoton', 'photonInfo')}</div>
-            <Col xsOffset={1} xs={10}>
-              <Validation.components.Input
-                id='input-device-id'
-                className='form-control'
-                type='text'
-                name='deviceId'
-                value=''
-                placeholder={i18n.getTranslation('addPhoton', 'deviceId')}
-                validations={['required', 'deviceId']}/>
-              <Validation.components.Input
-                id='input-access-token'
-                className='form-control'
-                type='text'
-                name='accessToken'
-                value=''
-                placeholder={i18n.getTranslation('addPhoton', 'accessToken')}
-                validations={['required', 'accessToken']}/>
-            </Col>
-            <Col xsOffset={5} xs={2}>
-              <Validation.components.Button className='btn btn-success'>{i18n.getTranslation('addPhoton', 'register')}</Validation.components.Button>
-            </Col>
-          </Validation.components.Form>
+          <Header/>
+          <Grid className="center button-register-center">
+            <Validation.components.Form onSubmit={this.handleSubmit.bind(this)} >
+              <Row>
+                <Col>
+                  <Validation.components.Input
+                    id='input-device-id'
+                    className='input-style'
+                    type='text'
+                    name='deviceId'
+                    value=''
+                    placeholder={i18n.getTranslation('addPhoton', 'deviceId')}
+                    validations={['required', 'deviceId']}/>
+                  <Validation.components.Input
+                    id='input-access-token'
+                    className='input-style'
+                    type='text'
+                    name='accessToken'
+                    value=''
+                    placeholder={i18n.getTranslation('addPhoton', 'accessToken')}
+                    validations={['required', 'accessToken']}/>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Validation.components.Button className='button-style add-photon-button'>{i18n.getTranslation('addPhoton', 'register')}</Validation.components.Button>
+                </Col>
+              </Row>
+            </Validation.components.Form>
+          </Grid>
           <Alert stack={{limit: 1}} />
         </div>
       );
