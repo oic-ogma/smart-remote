af46db1cc5af4780ed36f1364866061aa79f5484
dimda <daviskemlers@gmail.com>
2017-01-14 23:14:41 +0900

    17 - テスト送信のUXをよくした
    問題
    テスト送信する時に反応がなくて、UXはよくない。
    
    解決策
    テスト送信押された時にボタンを3秒disabledにして、送信の間にledを光らすようにした。

--- imports/ui/components/IrTest.jsx
+++ imports/ui/components/IrTest.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import i18n from 'meteor/universe:i18n';
 
 export default class IrTest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sending: false,
+    };
+    this.irTestSend = this.irTestSend.bind(this);
+  }
+
   irTestSend() {
+    this.setState({sending: true});
     Meteor.call("irTestSend", 'deviceId', 'accessToken');
+    setTimeout(function() { this.setState({sending: false}); }.bind(this), 3000);
   }
 
   render() {
+    let disabled = this.state.sending ? 'disabled' : '';
     return (
-          <button type="button" className="button-style button-register-margin"  onClick={this.irTestSend}>
-            {i18n.getTranslation('irTest', 'testButton')}
-          </button>
+        <button type='button' className='button-style button-register-margin' onClick={this.irTestSend} disabled={disabled}>
+          {i18n.getTranslation('irTest', 'testButton')}
+        </button>
     );
   }
 }
