7d74986d45c716b01d5a905bcbe2a99af45675eb
youmjww <b4234@oic.jp>
2017-01-14 17:43:12 +0900

    22 - 抜けているインポートの追加
    説明
    必要なインポートにインポート漏れがあった
    
    解決策
    必要なインポートを追加した

--- server/main.js
+++ server/main.js
@@ -3,4 +3,6 @@ import '/imports/api/emails/server/methods.js';
 import '/imports/api/users/server/methods.js';
 import '/imports/api/particle/server/methods.js';
 import '/imports/api/ButtonLayout/server/publications.js';
+import '/imports/api/ButtonLayout/ButtonLayout.js';
 import '/imports/api/ButtonLibrary/server/publications.js';
+import '/imports/api/ButtonLibrary/ButtonLibrary.js';
