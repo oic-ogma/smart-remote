a9ef56fef6814c3f4923d7a43bcef40568ae6610
gucchii <b4247@oic.jp>
2017-01-15 22:55:57 +0900

    85 - 404ページの作成   問題     不正なリンクからエラーページ飛ばす必要があった。   解決策     Error404Page.jsxの作成、routeへの追加、番犬の導入、topページヘのリンクを追加   備考     番犬の画像は著作権などを調べておりませんのでより良い画像やコメントがありましたら案をください。

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -7,6 +7,7 @@ import SignIn from '../../ui/layouts/SignIn.jsx';
 import ForgotPassword from '../../ui/layouts/ForgotPassword.jsx';
 import ResetPassword from '../../ui/layouts/ResetPassword.jsx';
 import Top from '../../ui/layouts/Top';
+import Error404Page from '../../ui/layouts/Error404Page';
 
 Meteor.startup( () => {
   render(
@@ -17,6 +18,7 @@ Meteor.startup( () => {
       <Route path="sign-in" component={SignIn}/>
       <Route path="forgot-password" component={ForgotPassword}/>
       <Route path="reset-password/:token/:language" component={ResetPassword}/>
+      <Route path="*" component={Error404Page}/>
     </Router>,
     document.getElementById( 'react-root' )
   );
