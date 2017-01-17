bcef3cead1ab741e01f8efbff32e63e9699e9c5c
gucchii <b4247@oic.jp>
2017-01-17 15:05:15 +0900

    89 - ログイン状態によるページの表示を変更 　問題 　　ログイン状態によるページのアクセス制限が必要だった。 　解決策 　　AddPhoton.jsxを参考にログイン状態によってページの表示が変更されるようにした。

--- imports/ui/layouts/ResetPassword.jsx
+++ imports/ui/layouts/ResetPassword.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import i18n from 'meteor/universe:i18n';
 import Header from '../components/Header';
 import Validation from 'react-validation';
-import { Grid } from 'react-bootstrap';
+import { Grid, Col } from 'react-bootstrap';
 import '../../api/validator/form_validator';
 import Alert from 'react-s-alert';
+import Loading from 'react-loading';
+import { browserHistory } from 'react-router';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
-export default class ResetPassword extends React.Component {
+export default class ResetPassword extends TrackerReact(React.Component) {
   constructor() {
     super(...arguments);
     this.state = {
@@ -53,35 +56,46 @@ export default class ResetPassword extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Header/>
-        <Grid className="center reset-password-center">
-          <Validation.components.Form onSubmit={this.handleSubmit.bind(this)}>
-            <Validation.components.Input
-              id='new-password'
-              className="input-style"
-              type='password'
-              value=''
-              name='password'
-              placeholder={i18n.getTranslation('resetPassword', 'newPassword')}
-              validations={['required', 'password']}
-            />
-            <Validation.components.Input
-              id='new-password-again'
-              className="input-style"
-              type='password'
-              value=''
-              name='confirmPassword'
-              placeholder={i18n.getTranslation('resetPassword', 'newPasswordAgain')}
-              validations={['required', 'confirmPassword']}
-            />
-            <Validation.components.Button className="button-style reset-password-button">{i18n.getTranslation('resetPassword', 'update')}</Validation.components.Button>
-          </Validation.components.Form>
-          <Alert stack={{limit: 1}} />
-        </Grid>
-      </div>
-    );
+    if (Meteor.loggingIn()) {
+      return (
+        <Col xsOffset={4} xs={4} mdOffset={4} md={4}>
+          <Loading type='bars' color='rgb(255, 255, 255)' />
+        </Col>
+      );
+    } else if (Meteor.user()) {
+      browserHistory.push('/my-page');
+      return null;
+    } else {
+      return (
+        <div>
+          <Header/>
+          <Grid className="center reset-password-center">
+            <Validation.components.Form onSubmit={this.handleSubmit.bind(this)}>
+              <Validation.components.Input
+                id='new-password'
+                className="input-style"
+                type='password'
+                value=''
+                name='password'
+                placeholder={i18n.getTranslation('resetPassword', 'newPassword')}
+                validations={['required', 'password']}
+              />
+              <Validation.components.Input
+                id='new-password-again'
+                className="input-style"
+                type='password'
+                value=''
+                name='confirmPassword'
+                placeholder={i18n.getTranslation('resetPassword', 'newPasswordAgain')}
+                validations={['required', 'confirmPassword']}
+              />
+              <Validation.components.Button className="button-style reset-password-button">{i18n.getTranslation('resetPassword', 'update')}</Validation.components.Button>
+            </Validation.components.Form>
+            <Alert stack={{limit: 1}} />
+          </Grid>
+        </div>
+      );
+    }
   }
 }
 
